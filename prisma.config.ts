d53194ef034d5634b9341e050a0f40eed74e1b9c
Omesh Mehta <omeshmehta70@gmail.com>
2026-07-23 11:46:04 +0530

    prima ci pipeliner error removed

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,9 +1,9 @@
 import "dotenv/config";
-import { defineConfig, env } from "@prisma/config";
+import { defineConfig } from "@prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
   datasource: {
-    url: env("Mamta_Associates_PRISMA_DATABASE_URL"),
+    url: process.env.Mamta_Associates_PRISMA_DATABASE_URL || "postgresql://dummy:dummy@localhost/dummy",
   },
 });
